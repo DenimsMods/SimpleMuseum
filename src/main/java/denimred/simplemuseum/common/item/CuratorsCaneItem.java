f14218f99dd4912e3bdbd4155fc385ba517a319c
Ryan <ryanvicks97@gmail.com>
2022-07-11 13:29:45 +0100

    Reverted CuratorsCaneItem to open the usual menu once again

--- src/main/java/denimred/simplemuseum/common/item/CuratorsCaneItem.java
+++ src/main/java/denimred/simplemuseum/common/item/CuratorsCaneItem.java
@@ -29,8 +29,7 @@ public class CuratorsCaneItem extends SimpleFoiledItem {
             if (!player.level.isClientSide) {
                 return InteractionResult.CONSUME;
             } else {
-                ClientUtil.openPuppetItemScreen((PuppetEntity) target, null);
-//                ClientUtil.openPuppetScreen((PuppetEntity) target, null);
+                ClientUtil.openPuppetScreen((PuppetEntity) target, null);
                 return InteractionResult.SUCCESS;
             }
         }
